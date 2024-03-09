a3000fcf7bc70decd3dcdf81054a008fbdd82b75
connkat <hi.connkat@gmail.com>
2024-03-09 16:34:13 -0700

    blind75 additions

--- isPalindrome.js
+++
@@ -1,8 +0,0 @@
-function isPalindrome (string) {
-  y = string.toString().split("").reverse().join("");
-  if (y == string) {
-    return true;
-  } else return false;
-};
-
-console.log(isPalindrome(101))
